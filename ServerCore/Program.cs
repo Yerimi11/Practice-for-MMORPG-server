8e96f4c6372fa7c2d5d9547d01e9e3ad988573a8
yerim <sontt4266@naver.com>
2025-09-24 23:28:39 +0900

    메모리 배리어 적용

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,22 +4,38 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // 메모리 배리어
+    // A) 코드 재배치 억제
+    // B) 가시성
+
+    // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘 다 막는다
+    // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
+    // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
+
     class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static volatile int x = 0;
+        static volatile int y = 0;
+        static volatile int r1 = 0;
+        static volatile int r2 = 0;
 
         static void Thread_1()
         {
             y = 1; // Store y
+
+            // -----------------------
+            Thread.MemoryBarrier();
+
             r1 = x; // Load x
         }
 
         static void Thread_2()
         {
             x = 1; // Store x
+
+            // -----------------------
+            Thread.MemoryBarrier();
+            
             r2 = y; // Load y
         }
 
