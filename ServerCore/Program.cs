946a1fb4cbc6bba0549facc0c9b431e8ba11fa0f
yerim <sontt4266@naver.com>
2025-10-22 23:32:02 +0900

    데드락 구현

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,6 +4,48 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -13,37 +55,17 @@ namespace ServerCore
         {
             for (int i = 0; i < 1000000; i++)
             {
-                // 상호배제 (Mutual Exclusion)
-                lock (_obj)
-                {
-                    // Monitor.Enter(_obj)
-                    number++;
-                    // Monitor.Exit(_obj)
-                }
-
-
-                //try
-                //{
-                //    Monitor.Enter(_obj); // lock (_obj)
-                //    number++;
-
-                //    return; // return 후 finally 실행됨
-                //}
-                //finally
-                //{
-                //    Monitor.Exit(_obj); // 잠금 해제
-                //}
+                SessionManager.Test();
             }
         }
+
+        // 데드락 Deadlock
 
         static void Thread_2()
         {
             for (int i = 0; i < 1000000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
